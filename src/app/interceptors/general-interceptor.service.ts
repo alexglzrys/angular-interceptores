537785edd3c43816113aba0bdd58e781fb8c9922
Alejandro González Reyes <alexglzrys@gmail.com>
2022-01-16 19:16:47 -0600

    implementar lógica del interceptor HTTP

--- src/app/interceptors/general-interceptor.service.ts
+++ src/app/interceptors/general-interceptor.service.ts
@@ -1,6 +1,7 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 /**
  * Los INTERCEPTORS son servicios tradicionales
@@ -21,13 +22,34 @@ export class GeneralInterceptorService implements HttpInterceptor {
 
   constructor() { }
 
+  // Lógica de intercepción de la petición HTTP
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    console.log('Paso por el interceptor');
+    // Crear el token (se enviará en todas las peticiones)
+    // Forma recomendada de agregar headers a una consulta
+    const headers = new HttpHeaders({
+      'token-usuario': 'sdshu4fd845f/*sd9s45dsd/*sd8s*?$3sds4'
+    });
+
+    // Las peticiones no se pueden modificar una vez construidas (Enviadas).
+    // En este caso se procede a clonarlas con la nueva configuración deseada
+    const reqClone = req.clone({
+      headers
+    });
 
     // Continuar con el flujo de la petición
-    return next.handle(req);
+    return next.handle(reqClone).pipe(
+      // Atrapar cualquier error (400 | 500) generado en el servidor
+      catchError(this.manejarError)
+    );
 
+  }
+
+  manejarError(err: HttpErrorResponse) {
+    console.log('Sucedio un error', 'Registrado en el log file')
+    console.warn(err);
+    // Mandar el error para que sea gestionado por el cliente que invoca este servicio
+    return throwError('Error personalizado')
   }
 
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,7 +1,6 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { throwError } from 'rxjs';
-import { catchError, map } from "rxjs/operators";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -18,33 +17,22 @@ export class UsuarioService {
     // Para agregar más cadenas de consulta se hace de la siguiente forma
     params = params.append('nombre', 'Alejandro González');
 
-    // Forma recomendada de agregar headers a una consulta
-    const headers = new HttpHeaders({
-      'token-usuario': 'sdshu4fd845f/*sd9s45dsd/*sd8s*?$3sds4'
-    });
 
 
     /**
      * Problema de peticiones HTTP tradicionales
-     * ? La mayoria de los endpoints requieren los mismos headers: código repetitivo
+     * ? La mayoria de los endpoints requieren los mismos headers TOKEN: código repetitivo
      * ! La forma en como se atrapan los errores, será la misma en todos los endpoints: código repetitivo
+     * Algo que siempre cambia para cada endpont, son los queryString que se le pueden enviar a la ruta
      *
      * - Lo mejor es usar interceptores, ya que esta lógica repetivia se concentra en un solo lugar.
      */
     return this.http.get(`https://reqres.in/api/users`, {
       params,
-      headers
     }).pipe(
       map((res: any) => {
         // Solo me interesa la data (contiene el listado de usuarios)
         return res['data'];
-      }),
-      catchError(err => {
-        // Atrapar cualquier error (400 | 500) generado en el servidor
-        console.log('Sucedio un error', 'Registrado en el log file')
-        console.warn(err);
-        // Mandar el error para que sea gestionado por el cliente que invoca este servicio
-        return throwError('Error personalizado')
       })
     );
   }
